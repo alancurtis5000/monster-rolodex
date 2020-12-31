632551f5081773797e9b533e1e62c715069d525c
Alan <alancurtis5000+accounts@gmail.com>
2020-12-30 18:37:43 -0700

    init commit basic app and card

---
+++ src/components/card-list/card-list.component.jsx
@@ -0,0 +1,14 @@
+import "./card-list.styles.css";
+import { Card } from "../card/card.component";
+
+export const CardList = (props) => {
+  console.log(props);
+
+  return (
+    <div className="card-list">
+      {props.monsters.map((monster, i) => (
+        <Card key={i} monster={monster}></Card>
+      ))}
+    </div>
+  );
+};
